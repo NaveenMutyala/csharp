6585e9754c1acd4be7fb7cccfc45837b6a309594
Naveen kumar Mutyala <81841128+NaveenMutyala@users.noreply.github.com>
2022-09-22 10:20:58 +0530

    Create extensionMethod.cs

---
+++ extensionMethod.cs
@@ -0,0 +1,55 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+namespace ExtensionMethods
+{
+    public class ExtClass{
+        public void test1()
+        {
+            Console.WriteLine("Test1 Method");
+        }
+        public void test2()
+        {
+            Console.WriteLine("Test2 Method");
+        }
+    }
+    public class child:ExtClass{
+      int i=10;
+        
+    }
+    
+    static class ExtensionClass
+    {
+        public static void test3(this ExtClass e)
+        {
+            Console.WriteLine("this is extension method");
+        }
+    }
+    
+
+    public class HelloWorld
+    {
+        
+        
+        public static void Main(string[] args)
+        {
+            ExtClass e= new ExtClass();
+            e.test1();
+            e.test2();
+            e.test3();
+            child c = new child();
+            c.test3();
+            
+        }
+    }
+}
+
+
+/*output:
+
+Test1 Method
+Test2 Method
+this is extension method
+this is extension method
+*/
